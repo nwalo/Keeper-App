7cc54879b4a6c23701f4a3032b1fb1b3ca4d06ae
Nwalo <nwalookechukwu@gmail.com>
2022-03-10 15:48:04 +0100

    remove unnecessary codes from todoItem.sx

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,16 +1,7 @@
-import react, { useState } from 'react';
+import react from 'react';
 
 function TodoItem(props) {
-	// const [ style, setStyle ] = useState(false);
-
-	// function handleClick(e) {
-	// 	if (style === false) {
-	// 		setStyle(true);
-	// 	} else {
-	// 		setStyle(false);
-	// 	}style={style ? { textDecoration: 'line-through' } : null}
-	// }
-
+	
 	return <li onClick={props.delete}>{props.text}</li>;
 }
 export default TodoItem;
